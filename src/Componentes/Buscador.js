c8b21b68a709fa7de562582d5020658cb20ce5f7
Hazael Lopez <elhazael134@gmail.com>
2020-07-15 10:08:44 -0600

    Creamos la funcion obtenerDatos = (ev) =>{} y se la pasamos al formulario

--- src/Componentes/Buscador.js
+++ src/Componentes/Buscador.js
@@ -4,10 +4,16 @@ class Buscador extends React.Component{
 
     busquedaRef = React.createRef();
 
+    obtenerDatos = (ev) =>{
+        ev.preventDefault();
+
+        console.log(this.busquedaRef.current.value)
+    }
+
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.obtenerDatos}>
                     <div className="row">
                         <div className="form-group col-md-8">
                             <input ref={this.busquedaRef} type="text" className="form-control form-control-lg" placeholder="Puedes hacer tu busqueda aquí" />
