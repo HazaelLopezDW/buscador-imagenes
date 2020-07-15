6429fae4910cb2f62685beb67c78a806347b9818
Hazael Lopez <elhazael134@gmail.com>
2020-07-15 15:41:04 -0600

    Terminamos con el pequeño video

--- src/App.js
+++ src/App.js
@@ -11,17 +11,43 @@ class App extends React.Component {
     pagina: ""
   }
 
+  scroll = () =>{
+    const elemento = document.querySelector('.jumbotron')
+    elemento.scrollIntoView('smooth', 'start')
+  }
+
   paginaAnterior = () =>{
-    console.log("Anterior...")
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // Si la pagina es uno ya no ir hacia atras
+    if(pagina === 1) return null;
+
+    // sumar uno a la pagian actual
+    pagina--
+    // agregar el cambio a state
+    this.setState({
+      pagina
+    }, () =>{ this.consultarApi()
+              this.scroll()})
   }
 
   paginaSiguiente = () =>{
-    console.log("Siguiente...")
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+    // sumar uno a la pagian actual
+    pagina++
+    // agregar el cambio a state
+    this.setState({
+      pagina
+    }, () =>{ this.consultarApi()
+              this.scroll()})
   }
 
   consultarApi = () =>{
     const termino = this.state.termino;
-    const url =  `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${this.state.termino}&per_page=32`;
+    const pagina = this.state.pagina;
+    const url =  `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${this.state.termino}&per_page=32&page=${pagina}`;
 
     fetch(url)
       .then(respuesta => respuesta.json())
@@ -30,7 +56,7 @@ class App extends React.Component {
 
   datosBusqueda = (termino) =>{
     this.setState({
-      termino: "termino",
+      termino: termino,
       pagina: 1
     }, () => { this.consultarApi() })
   }
