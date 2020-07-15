e726297018b4ae59e68ba619d74fa444b0f38146
Hazael Lopez <elhazael134@gmail.com>
2020-07-15 09:59:23 -0600

    Agregamos al input busquedaRef = React.createRef(); para hacer las busquedas

--- src/Componentes/Buscador.js
+++ src/Componentes/Buscador.js
@@ -10,7 +10,7 @@ class Buscador extends React.Component{
                 <form>
                     <div className="row">
                         <div className="form-group col-md-8">
-                            <input type="text" className="form-control form-control-lg" placeholder="Puedes hacer tu busqueda aquí" />
+                            <input ref={this.busquedaRef} type="text" className="form-control form-control-lg" placeholder="Puedes hacer tu busqueda aquí" />
                         </div>
                         <div className="form-group col-md-4">
                             <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar"/>
