1a77347655acdd879eeb9278c20498075e338cc0
Hazael Lopez <elhazael134@gmail.com>
2020-07-15 13:47:43 -0600

    Cambiamos nuestro &per_page=30 á &per_page=32

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
 
   consultarApi = () =>{
     const termino = this.state.termino;
-    const url =  `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${this.state.termino}&per_page=30`;
+    const url =  `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${this.state.termino}&per_page=32`;
 
     fetch(url)
       .then(respuesta => respuesta.json())
